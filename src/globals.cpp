96d10ba106f2c99e2db40255ad518fbfc3e007a5
karutimconver <karutimconver@gmail.com>
2025-03-31 20:20:34 +0100

    colors improved

--- src/globals.cpp
+++ src/globals.cpp
@@ -1,3 +1,3 @@
 #include "globals.hpp"
 
-const std::array<Color, 8> colors = {BLACK, RED, ORANGE, YELLOW, GREEN, BLUE, DARKBLUE, MAGENTA};
+const std::array<Color, 8> colors = {BLACK, RED, ORANGE, YELLOW, GREEN, {51, 255, 255, 255}, BLUE, MAGENTA};
